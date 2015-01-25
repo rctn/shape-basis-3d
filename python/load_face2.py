69c89307867c0acc342261e921144b6236bc8eb6
mudigonda <mayur.mudigonda@gmail.com>
2015-01-24 18:34:22 -0800

    If you remove the color stuff the rest of the script loads a face into blender and scales/translates itself and teh camera

--- python/load_face2.py
+++ python/load_face2.py
@@ -9,7 +9,7 @@ from math import radians
 jubf234=scio.loadmat('/media/mudigonda/Gondor/Projects/shape-basis-3d/matlab/JUBF234.mat')
 faces=jubf234['faces']
 vertices=jubf234['vertices']
-
+texture=jubf234['texture']
 for object in bpy.data.objects:
     if object.name != 'Lamp' and object.name !='Camera':
         object.select = True
@@ -39,6 +39,28 @@ mesh_3d.update()
 
 #creating a new object to link the object
 obj = bpy.data.objects.new("My_Object", mesh_3d)  
+mesh = obj.data
+vertex_colors = mesh.vertex_colors
+
+#Check that we have vertex_colors, we don't so we are creating a dummy one
+if len(vertex_colors)==0:
+    vertex_colors.new()
+else:
+    print("Vertex Colors Exist. Fuuuuck")
+
+color_layer = vertex_colors['Col']
+
+for ii in mesh.polygons:
+    for idx in poly.loop_indices:
+        texture=texture.flatten()
+        for jj in range(3):
+            rgb = double(texture[mesh.polygons[ii][idx]]
+            color_layer.data[idx].color = rgb
+
+mat = bpy.data.materials.new('vertex_material')
+mat.use_vertex_color_paint = True
+mat.use_vertex_color_light = True
+mesh.materials.append(mat)
 
 #we take the context
 scene = bpy.context.scene    
@@ -52,7 +74,10 @@ print(bpy.context.selected_objects)
 myObj = bpy.context.selected_objects[0]
 print(myObj)
 
-myObj.scale = ((0.1,0.1,0.1))
+#
+
+
+myObj.scale = ((0.025,0.025,0.025))
 
 ##This works if you run it through blender player's python engine but not so when you run it through a standalone script
 #translate
@@ -60,8 +85,10 @@ bpy.data.objects["My_Object"].location=(-5.0,-5.0,0.0)
 
 #camera
 #This works, since we used it previously
-bpy.data.objects['Camera'].location = (0.0,5.0,5.0)
-bpy.data.objects['Camera'].rotation_euler = (radians(90),0.0,0.0)
+bpy.data.objects['Camera'].location = (0.0,1.0,7.0)
+#bpy.data.objects['Camera'].rotation_euler = (radians(90.0),radians(90.0),radians(15.0))
+#bpy.data.objects['Camera'].rotation_euler = (radians(90.0),radians(90.0),radians(180.0))
+bpy.data.objects['Camera'].rotation_euler = (radians(0.0),radians(0.0),radians(180.0))
 fov=150
 scene.camera.data.angle = fov*(np.pi/180.0)
 
