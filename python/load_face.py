aa1a2dd01ee7c9075356352e63707e7db2b21586
mudigonda <mayur.mudigonda@gmail.com>
2015-02-02 21:23:44 -0800

    Trying to experimetn with various modifications to the faces (polygons)

--- python/load_face.py
+++ python/load_face.py
@@ -6,9 +6,12 @@ sys.path.append('//home/mudigonda/blender-git/build_opencolorio/bin/')
 import bpy
 from math import radians
 import random
+import pdb
 
-jubf234=scio.loadmat('/media/mudigonda/Gondor/Projects/shape-basis-3d/matlab/JUBF234.mat')
+#jubf234=scio.loadmat('/media/mudigonda/Gondor/Projects/shape-basis-3d/matlab/JUBF234.mat')
+jubf234=scio.loadmat('/media/mudigonda/Gondor/Projects/shape-basis-3d/matlab/JUHF248.mat')
 faces=jubf234['faces']
+faces = faces-1
 vertices=jubf234['vertices']
 texture=jubf234['texture']
 
@@ -36,12 +39,17 @@ for ii in range(faces.shape[0]):
    face.append((int(faces[ii,0]),int(faces[ii,1]),int(faces[ii,2])))
 
 #This lets us create our own mesh of an object
-mesh_3d.from_pydata(vertex, [], face)  
+mesh_3d.from_pydata(vertex, [], face)
 #We update the scene
 mesh_3d.update()
+#creating a new object to link the object
+obj = bpy.data.objects.new("My_Object", mesh_3d)
+#we take the context
+scene = bpy.context.scene
+#link the object to the scene
+scene.objects.link(obj)
 
 
-#We take the vertex colors
 vertex_colors = mesh_3d.vertex_colors
 
 if len(vertex_colors)==0:
@@ -49,34 +57,32 @@ if len(vertex_colors)==0:
 
 color_layer = vertex_colors['Col']
 i = 0
-print("Counting nonzero Texture elements")
-print(np.count_nonzero(texture))
-for poly in mesh_3d.polygons:
-    for idx in poly.loop_indices:
-        rgb = [random.random() for i in range(3)]
-#        color_layer.data[i].color = rgb
-#        if i<262144:
-        color_layer.data[i].color = rgb
-#            color_layer.data[i].color = texture[i,:]/255.0
-#        else:
-        i += 1
-#            print(i)
+non_zero = np.nonzero(texture)
 
+'''
+for poly in mesh_3d.polygons:
+    for idx in poly.vertices:
+        #rgb = list(np.random.random((3,1)))
+        #rgb = [0.59215,0.36470,.2]
+        rgb = list(texture[idx,:]/255.0)
+        color_layer.data[i].color = rgb
+        i += 1
+print("The final value of i is ",i)
+'''
+
+face_flat = faces.flatten()
+for ii in range(face_flat.shape[0]):
+    color_layer.data[ii].color = list(texture[face_flat[ii],:]/255.0)
+
+'''
+for ii in range(texture.shape[0]):
+    color_layer.data[ii].color = list(texture[ii,:]/255.0)
+'''
 mat = bpy.data.materials.new('vertex_material')
 mat.use_vertex_color_paint = True
 mat.use_vertex_color_light = True
 mesh_3d.materials.append(mat)
-mesh_3d.update()
 
-#creating a new object to link the object
-obj = bpy.data.objects.new("My_Object", mesh_3d)  
-
-#we take the context
-scene = bpy.context.scene    
-#link the object to the scene
-scene.objects.link(obj)    
-#we select the object, we want to work with objects over meshes. that be the idea
-obj.select = True    
 
 #Experimenting with trying to set active object, seems more or less useless or a duplicate of the previous statements
 for object in bpy.data.objects:
@@ -84,16 +90,11 @@ for object in bpy.data.objects:
         object.select = True
         bpy.context.scene.objects.active = object
 
-objs = [l for l in bpy.data.objects]
-print(objs)
 print(bpy.context.selected_objects)
-
 myObj = bpy.context.selected_objects[0]
 print(myObj)
 #Not sure what these next pair of statements do but they are important to edit object
 bpy.ops.object.mode_set(mode='OBJECT')
-#bpy.ops.object.mode_set(mode='EDIT')
-#bpy.ops.mesh.select_all(action='SELECT')
 
 ##Resize
 #myObj.scale = ((0.025,0.025,0.025))
@@ -117,5 +118,6 @@ scene.camera.data.angle = fov*(np.pi/180.0)
 
 #Rendering file
 path = "load_face2.png" 
+bpy.types.ImageFormatSettings.color_mode='RGB'
 bpy.data.scenes['Scene'].render.filepath = path
 bpy.ops.render.render(write_still=True)
