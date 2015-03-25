90328bc142a9f806b61ac1814d5b3ad5c04a47fa
mudigonda <mayur.mudigonda@gmail.com>
2015-03-24 20:43:24 -0700

    This version is trying to implement make_im without any loops so we can write a theano function without scans. This should in theory be possible. Some bugs need to be fixed

--- theano_code/pcd2im.py
+++ theano_code/pcd2im.py
@@ -61,6 +61,35 @@ def make_im(X, Y, Z, imSz=256):
 	
 	return im
 
+def make_im_noloop(X,Y,Z, binSz =256):
+    X = X.flatten()
+    Y = Y.flatten()
+    Z = Z.flatten()
+
+    Z = Z - Z.min() + 1300.0
+    x = X/Z
+    y = Y/Z
+
+    mnX, mxX =  x.min(), x.max()
+    mnY, mxY =  y.min(), y.max()
+    mn = np.min((mnX, mnY))
+    mx = np.max((mxX, mxY))
+
+    im = np.zeros((binSz*binSz))
+    new_x = (binSz-1)*(x - mn)/(mx-mn)
+    new_y = (binSz-1)*(y - mn)/(mx-mn)
+    '''
+    for ii in np.arange(x.shape[0]):
+        im[new_x[ii],new_y[ii]] = Z[ii]
+    '''
+    lin_idx = new_x*binSz + new_y
+    pdb.set_trace()
+    im[lin_idx.astype('uint8')] = Z
+
+    return im, new_x,new_y
+
+    
+
 
 def render_im(vertices, texture, imSz=256):
 	'''
